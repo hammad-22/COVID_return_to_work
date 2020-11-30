957fba6b5940fb225d16e616d55bcd6ed2a1bb50
kalebdagne <drakejosh>
2020-11-29 20:36:27 -0500

    Added checkboxes in Check In page

--- COVIDReturnToWork/app/src/main/java/com/example/covidreturntowork/CheckinFragment.kt
+++ COVIDReturnToWork/app/src/main/java/com/example/covidreturntowork/CheckinFragment.kt
@@ -1,14 +1,26 @@
 package com.example.covidreturntowork
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.CheckBox
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import java.lang.StringBuilder
 import androidx.lifecycle.Observer
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class CheckinFragment : Fragment() {
+
+    lateinit var pizza: CheckBox
+    lateinit var coffee: CheckBox
+    lateinit var burger: CheckBox
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
